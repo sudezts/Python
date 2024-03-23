f24e88dca1e2ee017265b0436b1031c94702f96a
Sude <119412170+sudezts@users.noreply.github.com>
2024-03-23 15:22:20 +0300

    Assignment

---
+++ Chapter_2.py
@@ -0,0 +1,14 @@
+#Things you should not do when assigning a name to a variable:
+#cannot start with a number
+#cannot contain spaces
+#Cannot contain symbols such as "*/(
+#cannot use reserved names such as input
+
+number=24
+print(number)
+
+number=25
+print(number)
+
+number+=1 #number=number+1
+print(number)
