76ddda56a4daedb4f90ac4a68b28157cf87ac12e
Sude <119412170+sudezts@users.noreply.github.com>
2024-03-23 15:42:29 +0300

    Data Types and Arithmetic Operators

---
+++ Chapter_3.py
@@ -0,0 +1,27 @@
+#DATA TYPES
+#int-->integer numbers -->12, 20
+#float -->decimal numbers -->2.5, 0.7
+#str --> string --> "Sude", 'but', 'a'
+#bool -->boolean -->true,false
+#list -->[1,2,true,'a']
+#set -->{1,2,True}
+#dict --> dictionary --> {'name':'Sude','age':20}
+#tup --> tuple -->(1,2,True)
+
+#ARITHMETIC OPERATORS
+print(5)
+print(type(3)) #<class 'int'>
+print(type(1.75)) #<class 'float'>
+print(3+5) #8
+print(3/5) #0.6
+print(3*5) #15
+print(3-5) #-2
+print(3**5) #3*3*3*3*3=243
+print(5%3) #2
+print(15//3) #5
+
+print(3+2*5) #13
+print((3+2)*5) #25
+
+print(8.2*2.5) #20.5
+print(5*3.2) #16.0
