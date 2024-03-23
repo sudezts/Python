0658652c973bc5bbd69468010993514d3bb648d1
Sude <119412170+sudezts@users.noreply.github.com>
2024-03-23 16:51:28 +0300

    Tuple

---
+++ Chapter_7.py
@@ -0,0 +1,11 @@
+#TUPLE
+#Similar with List
+#But we can't change elements in tuple
+tup = ('a','b','c','d','e','a')
+print(tup) #('a', 'b', 'c', 'd', 'e', 'a')
+
+print(tup[0]) #a
+
+print(tup.count('a')) #2
+
+print(tup.index('e')) #4
