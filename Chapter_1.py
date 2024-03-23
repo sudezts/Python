0a9541efaa6846c0b25ec507301fc5d055a883d0
Sude <119412170+sudezts@users.noreply.github.com>
2024-03-23 15:12:26 +0300

    Hello World!

---
+++ Chapter_1.py
@@ -0,0 +1,10 @@
+print("Hello world")
+print("Hello world! \nMy name is Sude.") #\n is used to move to the next line.
+print("My favorite song: \tBillie Eilish-What Was I Made For?") #\t leaves a space as large as the characters.
+
+#format
+
+print("My name is {}".format("Sude")) #format command allows filling in {}
+print("My name is {} and I am {} years old.".format("Sude",20))
+print("I am {1} years old and My name is {0}.".format("Sude",20))
+print("My name is {name} and I am {age} years old.".format(name="Sude",age=20))
