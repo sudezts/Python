7e562287f0997fe3bbf61a0c38aa009bb651513f
Sude <119412170+sudezts@users.noreply.github.com>
2024-03-23 16:15:34 +0300

    Boolean

---
+++ Chapter_5.py
@@ -0,0 +1,26 @@
+a=True
+b=False
+print(a)
+print(b)
+
+num1=20
+num2=18
+print(num1>17) #True
+print(num1>num2) #True
+print(num1<num2) #False
+
+print(num1==num2) #False
+print(num1==20) #True
+
+print(num1<=21) #True
+print(num1>=23) #False
+
+print(num1<=20) #True
+print(num1>=20) #True
+
+print(num1!=10) #True
+print(num1!=20) #False
+
+print(not num1<17) #True
+print(not num1==20) #False
+print(not num1<=20) #false
