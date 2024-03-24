9ef8fe33bbee2020e033280eac09330c6ee0b511
Sude Öztaş <119412170+sudezts@users.noreply.github.com>
2024-03-24 16:46:10 +0300

    Zip

---
+++ Chapter_14.py
@@ -0,0 +1,12 @@
+#Zip
+
+avengers=["Bucky Barnes","Tony Stark","Natasha Romanoff","Steve Roger"]
+
+sss=range(1,5)
+
+for x in zip(avengers, sss):
+    print(x)
+# ('Bucky Barnes', 1)
+# ('Tony Stark', 2)
+# ('Natasha Romanoff', 3)
+# ('Steve Roger', 4)
